32865bfd6f762e12080c2e216f67cad404a31fae
playpotel <playpotel@gmail.com>
2026-02-03 05:59:05 +0700

    Delete functions/article directory

--- functions/article/[id].js
+++
@@ -1,8 +0,0 @@
-export async function onRequest({ request, env }) {
-  const url = new URL(request.url);
-
-  // arahkan ke /article/index.html (AMAN dari Clean URL redirect)
-  url.pathname = '/article/index.html';
-
-  return env.ASSETS.fetch(new Request(url.toString(), request));
-}

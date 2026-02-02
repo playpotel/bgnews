846764c5dd03329d39cb46888e5356a2395c736d
playpotel <playpotel@gmail.com>
2026-02-03 05:27:03 +0700

    Update [id].js

--- functions/article/[id].js
+++ functions/article/[id].js
@@ -1,3 +1,8 @@
-export async function onRequest(context) {
-  return context.env.ASSETS.fetch(context.request);
+export async function onRequest({ request, env }) {
+  const url = new URL(request.url);
+
+  // paksa semua /article/* diarahkan ke /article.html
+  url.pathname = '/article.html';
+
+  return env.ASSETS.fetch(new Request(url.toString(), request));
 }
